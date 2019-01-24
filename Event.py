5f264b9da88fb8d8ff0f1a35641dad72543a5bc0
ntrung1008 <trung_nguyen1008@yahoo.com>
2019-01-23 19:28:46 -0800

    Added more variety for event (trivia and math)

--- Event.py
+++ Event.py
@@ -8,6 +8,13 @@ class Energy(Event):
     def __init__(self,name):
         super().__init__(name)
     def trigger(self,current_energy,increase_by):
+        rng = random.randint(1, 2)
+        if rng ==1:
+            return self.trigger_math(current_energy,increase_by)
+        if rng == 2:
+            return self.trigger_trivia(current_energy,increase_by)
+
+    def trigger_math(self,current_energy,increase_by):
         rng=random.randint(1,3)
         if rng==1:
             number1= random.randint(1,100)
@@ -39,6 +46,34 @@ class Energy(Event):
                 current_energy = current_energy + increase_by
             else:
                 print("Incorrect, you miss your chance")
+        return current_energy
+
+    def trigger_trivia(self, current_energy,increase_by):
+        rng = random.randint(1, 3)
+        if rng==1:
+            print("In C++, how do you terminate a statement ?")
+            answer = input()
+            if answer ==';':
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy=current_energy+increase_by
+            else:
+                print("Incorrect. You miss your chance")
+        if rng == 2:
+            print("In C++, how do you dereference a pointer ?")
+            answer = input()
+            if answer == '&':
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+        if rng == 3:
+            print("In C++, what is the symbol for pointer ?")
+            answer = input()
+            if answer == '*':
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
         return current_energy
 
 
