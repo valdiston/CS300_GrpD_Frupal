8f612a18fe967533d8abe54d1abf5ba8bb7a0daa
ntrung1008 <>
2019-01-27 13:27:57 -0800

    Added 5 more trivia questiosn and Rock paper scissor events

--- Event.py
+++ Event.py
@@ -7,7 +7,7 @@ class Event:
 class Energy(Event):
     def __init__(self,name):
         super().__init__(name)
-    def trigger(self,current_energy,increase_by):
+    def trigger(self,current_energy,increase_by): #Randomly play 1 of the 3 type of event: Trivia, math or sudden death
         rng = random.randint(1, 3)
         if rng ==1:
             return self.trigger_math(current_energy,increase_by)
@@ -15,6 +15,13 @@ class Energy(Event):
             return self.trigger_trivia(current_energy,increase_by)
         if rng == 3:
             return self.trigger_death(current_energy,increase_by)
+
+    def trigger_no_sudden_death(self,current_energy,increase_by):#Randomly play 1 of the 2 type of even: Trivia or math
+        rng = random.randint(1, 2)
+        if rng ==1:
+            return self.trigger_math(current_energy,increase_by)
+        if rng == 2:
+            return self.trigger_trivia(current_energy,increase_by)
 
     def trigger_math(self,current_energy,increase_by):
         rng=random.randint(1,3)
@@ -84,6 +91,78 @@ class Energy(Event):
                 current_energy = current_energy + increase_by
             else:
                 print("Incorrect. You miss your chance")
+        if rng == 5 :
+            print("Which bird can fly backward ?")
+            answer =input()
+            if answer.upper() == "Hummingbird":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
+        if rng == 6:
+            print("What type of animal is a wahoo?")
+            answer = input()
+            if answer.upper() == "Fish":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
+        if rng == 6:
+            print("What type of animal is a horny toad?")
+            answer = input()
+            if answer.upper() == "Lizard":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
+        if rng == 7:
+            print("What type of animal is a mountain chicken ?")
+            answer = input()
+            if answer.upper() == "Frog":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
+        if rng == 8:
+            print("WWhat letter is defined as the square root of -1?")
+            answer = input()
+            if answer == "i":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
+        if rng == 9:
+            print("What is the weight of 1 liter of water in kilogram ?")
+            answer = input()
+            if answer == "1":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
+        if rng == 9:
+            print("What is the name of the university that Bill Gate dropped out of")
+            answer = input()
+            if answer.upper() == "Havard":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
+        if rng == 10:
+            print("In a regular deck of cards, which is the only king without a moustache ?")
+            answer = input()
+            if answer.upper() == "Heart":
+                print("Correct. Your energy is increased by: ", increase_by)
+                current_energy = current_energy + increase_by
+            else:
+                print("Incorrect. You miss your chance")
+
         return current_energy
     def trigger_death(self, current_energy,increase_by):
         rng = random.randint(1, 3)
@@ -97,7 +176,7 @@ class Energy(Event):
             else:
                 print("The cute animal turns out to be a poisonous. You are lucky")
         if rng == 2:
-            print("You found the game developers. Do you think they are the smartest people ever Y/N")
+            print("You found the game developers. Do you want to die? Y/N")
             answer = input()
             if answer.upper() == "N":
                 print("You got brutally murdered . Game over")
@@ -116,12 +195,69 @@ class Energy(Event):
                 current_energy = 0
         return current_energy
 
+    def trigger_rock_paper_scissor(self, current_energy, increase_by):
+        print("In this event, you have to play a game of rock, paper, scissor")
+        print("Please choose what you would like to play. R for rock. P for paper. Anything else for scissor")
+        answer=input();
+
+        #Assign hand for user
+        if answer.upper() == "R":
+            user = "Rock"
+        elif answer.upper() == "P":
+            user = "Paper"
+        else:
+            user = "Scissor"
+
+        #Assign hand for computer
+        rng = random.randint(1, 3)
+        if rng == 1:
+            computer= "Rock"
+        if rng == 2:
+            computer = "Paper"
+        if rng == 3:
+            computer = "Scissor"
+
+         #Compare the result
+        if user =="Rock":
+            if computer == "Rock":
+                print("It's a draw but you still get energy for your effort")
+                current_energy = current_energy+increase_by
+            elif computer == "Paper":
+                print("You played Rock and the computer played Paper. You lose ")
+            else:
+                print("You played Rock and the computer played Scissor. You win")
+                current_energy = current_energy + increase_by
+
+        elif user == "Paper":
+            if computer == "Paper":
+                print("It's a draw but you still get energy for your effort")
+                current_energy=current_energy+increase_by
+            elif computer =="Rock":
+                print("You played Paper and the computer played Rock. You win !!")
+                current_energy = current_energy + increase_by
+            else:
+                print("You played Paper and the computer played Scissor. You lose")
+
+
+        if user == "Scissor":
+            if computer == "Scissor":
+                print("It's a draw but you still get energy for your effort")
+                current_energy=current_energy+increase_by
+            elif computer =="Paper":
+                print("You played Scissor and the computer played Paper. You win !!")
+                current_energy = current_energy + increase_by
+            else:
+                print("You played Scissor and the computer played Rock. You lose")
+        return current_energy
+
+
+
 
 def main():
     Energy1 = Energy("Event")
     current_energy =50
     increase_by =15
-    current_energy=Energy1.trigger(current_energy, increase_by)
+    current_energy=Energy1.trigger_rock_paper_scissor(current_energy, increase_by)
     print("Your current energy is: ",current_energy)
     if current_energy == 0:
         print("You're dead")
