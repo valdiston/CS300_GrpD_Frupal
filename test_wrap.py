818a96b21980c20a71dbb4a0514ebce40c5f8623
Julian Baker <bjulian@pdx.edu>
2019-01-28 20:09:18 -0800

    Updated test wrapper with a skeleton for checking reference map during movement

--- test_wrap.py
+++ test_wrap.py
@@ -8,6 +8,12 @@ island_map  = [ [0]* MAP_SIZE for i in range(MAP_SIZE) ]
 """ Status of Hero """
 coordinates = [[0],[0]]
 
+""" Obstacle Dict"""
+Obs = {"0": {"energy": 1, "item": "", "item energy": ""},
+       "w": {"energy": 2, "item": "boat", "item energy": 0},
+       "g": {"energy": 2, "item": "", "item energy": ""},
+       "s": {"energy": 2, "item": "", "item energy": ""},
+       "҉": {"energy": 1, "item": None, "item energy": ""}}
 
 def display():
     for i in range(0, MAP_SIZE):
@@ -32,18 +38,22 @@ def controls():
 def checkObstacle(coordniates, reference): #skeleton of a function to check energy needed for a move based on object
 
         # nextPosit = reference[current_x][current_y]
-        if reference_map[coordinates[0][0]][coordinates[1][0]] == 0:
-            return 1
-        elif reference_map[coordinates[0][0]][coordinates[1][0]] == 'w':
-            return 2
-        elif reference_map[coordinates[0][0]][coordinates[1][0]] == 'g':
-            return 1
-        elif reference_map[coordinates[0][0]][coordinates[1][0]] == 's':
-            return 2
-        elif reference_map[coordinates[0][0]][coordinates[1][0]] == '҉':
-            return 1
-        else:
-            return 1
+        # if reference_map[coordinates[0][0]][coordinates[1][0]] == 0:
+        #     return 1
+        # elif reference_map[coordinates[0][0]][coordinates[1][0]] == 'w':
+        #     return 2
+        # elif reference_map[coordinates[0][0]][coordinates[1][0]] == 'g':
+        #     return 1
+        # elif reference_map[coordinates[0][0]][coordinates[1][0]] == 's':
+        #     return 2
+        # elif reference_map[coordinates[0][0]][coordinates[1][0]] == '҉':
+        #     return 1
+        # else:
+        #     return 1
+        # if Obs[reference_map[coordinates[0][0]][coordinates[1][0]]]["item"] is None:
+        #     print("This obj doesn't require an item. obj = ", str(reference_map[coordinates[0][0]][coordinates[1][0]]),
+        #           str(Obs[reference_map[coordinates[0][0]][coordinates[1][0]]]))
+        return Obs[reference_map[coordinates[0][0]][coordinates[1][0]]]["energy"]
 
 
 def main():    
@@ -77,7 +87,7 @@ def main():
                 cost = checkObstacle(coordinates[0][0]-1, reference_map)
                 if cost < energy:
                     energy -= cost
-                    print("This move cost you" + str(cost) + " energy. You have " + str(energy) + " energy left")
+                    print("This move cost you " + str(cost) + " energy. You have " + str(energy) + " energy left")
                     coordinates[0][0] = coordinates[0][0] - 1
                 else:
                     print("You don't have enough energy to mover here")
