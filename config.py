8709912f8b78a82f384f7022151d40485c4af7ed
Leesha Laforteza <leesha@pdx.edu>
2019-02-11 14:22:36 -0800

    CSV read/write for admin works for string,integer pairs. TODO: Add all variables that have a cost

--- config.py
+++ config.py
@@ -11,31 +11,30 @@ import csv
 
 # Read current config file into a dictionary
 def edit_csv():
-	with open('config.csv', mode='r') as infile:
+	with open('config.csv', mode='r', newline='') as infile:
 		reader = csv.reader(infile)
-	with open('config.csv', mode = 'w' as outfile):
-		writer = csv.writer(outfile)
-		myDict = {rows[0]:rows[1] for rows in reader)
+	# with open('config.csv', mode = 'w') as outfile:
+		# writer = csv.writer(outfile)
+		next(reader)
+		myDict = {rows[0]:rows[1] for rows in reader}
 	for k,v in myDict.items():
 		# Output each dictionary item
-		if !str.isdigit(v):
-			# This just catches the first row so the user can't
-			# edit the headers, since the value isn't an integer
-			
-		print(k + ": " v + '\n')
-		# Enter a number to change the value
-		v = input("Please input the new value, or re-enter the old value.")
-	# Call the function to write the file
+		if str.isdigit(v):
+			print(k + ": " + v + '\n')
+			# Enter a number to change the value
+			newVal = input("Please input the new value, or re-enter the old value: ")
+			myDict[k] = newVal
+		# Call the function to write the file
 	write_csv(myDict)
 
 # Write the dictionary with the new values into the CSV file 
 # by passing in the dictionary (myDict)
 def write_csv(x):
 	with open('config.csv', mode = 'w') as csv_file:
-	fieldnames = ['item', 'value']
-		new_writer = csv.DictWriter(csv_file, fieldnames=fieldnames);
-	new_writer.writeheader()
-	for k,v in x.items():
-		new_writer.writerow('item': k, 'value': v)
+		fieldnames = ['item', 'value']
+		new_writer = csv.DictWriter(csv_file,delimiter=',', lineterminator='\n', fieldnames=fieldnames)
+		new_writer.writeheader()
+		for k,v in x.items():
+			new_writer.writerow({'item': k, 'value': v})
 
 # End File
---
+++ config_test.py
@@ -0,0 +1,6 @@
+from config import *
+
+def main():
+	edit_csv()
+	
+main()
