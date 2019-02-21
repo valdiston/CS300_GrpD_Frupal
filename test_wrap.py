8e405db5da4a7bd936cef9f9b78050fb7a1cd135
Julian Baker <bjulian@pdx.edu>
2019-02-20 20:09:22 -0800

    Added some commented code to test_wrap for testing clues

--- test_wrap.py
+++ test_wrap.py
@@ -1,8 +1,9 @@
 from mapMaker import *
 import Player
+import clueMaker
 
 """ Map Configurations """
-MAP_SIZE = 20
+MAP_SIZE = 5
 
 # dummy list of user added terrain
 extraTerrain = ["forest", "rocks"]
@@ -10,6 +11,9 @@ reference_map = mapMaker(MAP_SIZE, extraTerrain)
 
 """ Manual Test Map """
 # reference_map = [['e'] * MAP_SIZE for i in range(MAP_SIZE)]
+# reference_map[4][4] = 'j'
+# reference_map[4][3] = 'j'
+# reference_map[0][1] = 'c'
 
 island_map  = [ [0]* MAP_SIZE for i in range(MAP_SIZE) ]
 
@@ -23,6 +27,18 @@ def display():
             if i == coordinates[0][0] and j == coordinates[1][0]:
                 print('웃', end =' ')
             elif abs(i - coordinates[0][0] ) < 2 and abs(j - coordinates[1][0]) < 2:
+                                print(str(reference_map[i][j]), end = ' ')
+            else:
+                print(str(' '), end=' ')
+        print("")
+
+
+def testDisplay(coords, reference_map):
+    for i in range(0, MAP_SIZE):
+        for j in range(0, MAP_SIZE):
+            if i == coords[0] and j == coords[1]:
+                print('웃', end =' ')
+            elif abs(i - coords[0] ) < 2 and abs(j - coords[1]) < 2:
                                 print(str(reference_map[i][j]), end = ' ')
             else:
                 print(str(' '), end=' ')
@@ -44,6 +60,13 @@ def main():
     # dictList = [{"e":"event"}, {"c":"clue"}, {"g":"grass"}]
     # p.initKeys(dictList)
 
+    """ Testing clues """
+    # reference_map = [['e'] * MAP_SIZE for i in range(MAP_SIZE)]
+    # reference_map[4][4] = 'j'
+    # reference_map[4][3] = 'j'
+    #
+    # jewelList, clueList, terrainList, jewelString, terrainString = clueMaker.generateClues(reference_map)
+    # reference_map = clueMaker.clueMap(clueList, reference_map)
 
 
 
@@ -71,6 +94,9 @@ def main():
 
         #Walking North
         elif choice == 'w':
+            """ Testing Events """
+            # if p.location[1] > 0:
+            #     p.move_to([p.location[0], p.location[1] - 1], reference_map, clueList)
             if coordinates[0][0] > 0:
                 # cost = checkObstacle(coordinates[0][0]-1, reference_map)
                 cost = 0
@@ -86,9 +112,9 @@ def main():
                 
         #Walking South
         elif choice == 's':
-            """ Testing Events """
+            # """ Testing Events """
             # if p.location[1] < MAP_SIZE - 1:
-            #     p.move_to([p.location[0],p.location[1] + 1], reference_map)
+            #     p.move_to([p.location[0],p.location[1] + 1], reference_map, clueList)
             if coordinates[0][0] < MAP_SIZE - 1:
                 # cost = checkObstacle(coordinates[0][0] + 1, reference_map)
                 cost = 0
@@ -105,6 +131,9 @@ def main():
                 
         #Walking East
         elif choice == 'a':
+            """ Testing Events """
+            # if p.location[0] > 0:
+            #     p.move_to([p.location[0] - 1, p.location[1]], reference_map, clueList)
             if coordinates[1][0] > 0:
                 # cost = checkObstacle(coordinates[1][0] - 1, reference_map)
                 cost = 0
@@ -121,6 +150,9 @@ def main():
                 
         #Walking West
         elif choice == 'd':
+            """ Testing Events """
+            # if p.location[0] < MAP_SIZE - 1:
+            #     p.move_to([p.location[0] + 1, p.location[1]], reference_map, clueList)
             if coordinates[1][0] < MAP_SIZE - 1:
                 # cost = checkObstacle(coordinates[1][0] - 1, reference_map)
                 cost = 0
@@ -139,6 +171,7 @@ def main():
             print("Try a valid command""")
             
         display()
+        # testDisplay(p.location, reference_map)
 
 
 if __name__ == '__main__':
