b7c9cf8db040c2b22e589b004efa0eb9513d2fad
Julian Baker <bjulian@pdx.edu>
2019-02-28 08:53:56 -0800

    Removing pycache files from git since they cause issues

--- mazeMaker.py
+++ mazeMaker.py
@@ -3,9 +3,9 @@ import csv
 with open('easyGame.csv', 'r') as f:
     csv_reader = csv.reader(f)
 
-    #first read in the number of objects in maze
-    #then read in size of maze
-    #then read in the objects 
+    # first read in the number of objects in maze
+    # then read in size of maze
+    # then read in the objects
     for row in csv_reader:
         if row == 0:
             mazeSize = {row[0]}
@@ -13,6 +13,7 @@ with open('easyGame.csv', 'r') as f:
             objectCount = {row[0]}
         if row == 2:
             for x in (0, objectCount):
+                pass
 
 
 
@@ -20,4 +21,5 @@ with open('easyGame.csv', 'r') as f:
 T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
 for r in T:
     for c in r:
-        #function to randomly select object for maze element        
+        pass
+        # function to randomly select object for maze element
